645d09c9fdc9682440bcebd15104d54efc023cc9
Jonghwan Shin <sjh1338@naver.com>
2019-04-02 12:20:29 +0000

    crash avoidance completed

--- topics_quiz/src/detect_obstacles.py
+++ topics_quiz/src/detect_obstacles.py
@@ -1,25 +1,49 @@
 #! /usr/bin/env python
 
 import rospy
+import numpy as np
 from sensor_msgs.msg import LaserScan
 from geometry_msgs.msg import Twist
 
+check_range_length = 0
+
+# avoid obstacle
+# state
+# forward
+# avoid
+# right
+
 
 def callback(msg):
-    print msg.ranges
-    count.linear.x = 0.5
-    count.angular.z = 0.5
-    pub.publish(count)
-    rate.sleep()
+    range_front = msg.ranges[350:370]
+    range_right = msg.ranges[0:10]
+    range_left = msg.ranges[709:719]
+    d_left = np.average(range_left)
+    d_front = np.average(range_front)
+    d_right = np.average(range_right)
+    print "Dists at {0}: left-{1} front-{2} right-{3}".format(
+        msg.header.stamp, d_left, d_front, d_right)
+    if(d_front < 1):
+        print "EMERGENCY STOP ENGAGED"
+        move.linear.x = 0.1
+        move.angular.z = 5
+    else:
+        if(d_front == d_left == d_right == np.inf):
+            move.linear.x = 0.1
+            move.angular.z = -5
+        else:
+            move.linear.x = 0.5
+            move.angular.z = 0
+    pub.publish(move)
 
 
 rospy.init_node('topics_quiz_node')
 sub = rospy.Subscriber('/kobuki/laser/scan', LaserScan, callback)
 pub = rospy.Publisher('/cmd_vel', Twist, queue_size=1)
-rate = rospy.Rate(2)
-count = Twist()  # need to change to match the described type
-count.linear.x = 0
-count.angular.z = 0
+rate = rospy.Rate(1)
+move = Twist()  # need to change to match the described type
+move.linear.x = 0
+move.angular.z = 0
 rospy.spin()
 
 # while not rospy.is_shutdown():
